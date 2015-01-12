b4926449586d0b60c1bacc172ac570ec7b5909c9
grazzZapper <david.grasand@yahoo.se>
2015-01-12 15:36:48 +0100

    Updated Project Structure. Added Items.

--- src/main/java/com/zyberholes/procon/ProConMod.java
+++ src/main/java/com/zyberholes/procon/ProConMod.java
@@ -2,8 +2,14 @@ package com.zyberholes.procon;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import com.zyberholes.procon.entities.EntityBolt;
+import com.zyberholes.procon.items.Bolt;
+import com.zyberholes.procon.items.IronNugget;
+import com.zyberholes.procon.items.ScrewBase;
+import com.zyberholes.procon.items.ScrewNut;
 import com.zyberholes.procon.proxy.ProxyCommon;
 
 import cpw.mods.fml.common.Mod;
@@ -13,28 +19,59 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid = ProConMod.MODID, version = ProConMod.VERSION)
+@Mod(modid = Reference.MODID, name = "Project Construct", version = Reference.VERSION)
 public class ProConMod
 {
-	public static final String MODID = "procon";
-	public static final String VERSION = "0.1";
-	
-	@Instance(value = MODID)
+	@Instance(value = Reference.MODID)
 	public static ProConMod instance;
+	
+	//define mod content
+	public static Item bolt;
+	public static Item screwBase;
+	public static Item screwNut;
+	public static Item ironNugget;
 	
 	@SidedProxy(clientSide = "com.zyberholes.procon.proxy.ProxyClient", serverSide = "com.zyberholes.procon.proxy.ProxyCommon")
 	public static ProxyCommon proxy;
 	
+	//where most creation takes place!
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		GameRegistry.addRecipe(new ItemStack(Blocks.wool, 4), "DD", "DD", 'D', Blocks.dirt);
-		GameRegistry.addRecipe(new ItemStack(Items.flint_and_steel, 1), "x ", " x", 'x', Blocks.dirt);
+		
+		bolt = new Bolt();
+		screwBase = new ScrewBase();
+		screwNut = new ScrewNut();
+		ironNugget = new IronNugget();
+		
+		//register items:
+		GameRegistry.registerItem(bolt, "bolt");
+		GameRegistry.registerItem(screwBase, "screwBase");
+		GameRegistry.registerItem(screwNut, "screwNut");
+		GameRegistry.registerItem(ironNugget, "ironNugget");
+		
+		//shaped crafting:
+		GameRegistry.addRecipe(new ItemStack(bolt, 2), new Object[] {"nnn", " b ", " b ", 'n', ironNugget, 'b', screwBase});
+		GameRegistry.addRecipe(new ItemStack(screwBase, 2), new Object[] {"n ", " n", "n ", 'n', ironNugget});
+		GameRegistry.addRecipe(new ItemStack(screwBase, 2), new Object[] {" n", "n ", " n", 'n', ironNugget});
+		GameRegistry.addRecipe(new ItemStack(screwNut, 2), new Object[] {" nn", "n n", "nn ", 'n', ironNugget});
+		GameRegistry.addRecipe(new ItemStack(screwNut, 2), new Object[] {"nn ", "n n", " nn", 'n', ironNugget});
+		GameRegistry.addRecipe(new ItemStack(Items.iron_ingot, 1), new Object[] {"iii", "iii", "iii", 'i', ironNugget});
+		
+		//shapeless crafting:
+		GameRegistry.addShapelessRecipe(new ItemStack(ironNugget, 9), Items.iron_ingot);
+		
+		//smelting:
+		GameRegistry.addSmelting(bolt, new ItemStack(ironNugget, 3), 0F);
+		GameRegistry.addSmelting(screwBase, new ItemStack(ironNugget, 1), 0F);
+		GameRegistry.addSmelting(screwNut, new ItemStack(ironNugget, 3), 0F);
 	}
 	
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
+		EntityRegistry.registerModEntity(EntityBolt.class, "entityBolt", 4, this, 80, 3, true);
 		proxy.registerRenderers();
 	}
 	
--- src/main/java/com/zyberholes/procon/proxy/ProxyClient.java
+++ src/main/java/com/zyberholes/procon/proxy/ProxyClient.java
@@ -1,10 +1,17 @@
 package com.zyberholes.procon.proxy;
 
+import net.minecraft.client.renderer.entity.RenderSnowball;
+
+import com.zyberholes.procon.ProConMod;
+import com.zyberholes.procon.entities.EntityBolt;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
 public class ProxyClient extends ProxyCommon {
 	
 	@Override
 	public void registerRenderers() {
-		
+		RenderingRegistry.registerEntityRenderingHandler(EntityBolt.class, new RenderSnowball(ProConMod.bolt));
 	}
 	
 }
